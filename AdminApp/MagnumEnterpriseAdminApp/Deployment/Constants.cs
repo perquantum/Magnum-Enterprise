99764d769590e0803b28ce0f6a9761740513111c
Paresh <perquantum@gmail.com>
2026-02-10 14:08:01 +0530

    Updated path locations

--- AdminApp/MagnumEnterpriseAdminApp/Deployment/Constants.cs
+++ AdminApp/MagnumEnterpriseAdminApp/Deployment/Constants.cs
@@ -3,10 +3,10 @@
 public static class Constants
 {
     public const string AppSettingsFile = "appsettings.json";
-    public const string ReactLocalDistDirectory = @"D:\Projects\Magnum-Enterprise\WebApplications\ReactApp\magnum-app\dist";
+    public const string ReactLocalDistDirectory = @"D:\Magnum-Enterprise\ReactApp\magnum-app\dist";
     public const string DeploymentFailedMessage = "FTP deployment failed";
     public const string ReactBuildFailed = "React build failed";
-    public const string ReactWorkingDirectory = @"D:\Projects\Magnum-Enterprise\WebApplications\ReactApp\magnum-app";
+    public const string ReactWorkingDirectory = @"D:\Magnum-Enterprise\ReactApp\magnum-app";
 
     public static class FTP
     {
@@ -19,8 +19,8 @@ public static class Constants
 
     public static class ProductsJsonDirectory
     {
-        public const string AdminJson = @"D:\Projects\Magnum-Enterprise\AdminApplication\MagnumEnterpriseAdminApp\MagnumEnterpriseAdminApp\wwwroot\Products.json";
-        public const string ReactJson = @"D:\Projects\Magnum-Enterprise\WebApplications\ReactApp\magnum-app\src\data\products.json";
+        public const string AdminJson = @"D:\Magnum-Enterprise\AdminApp\MagnumEnterpriseAdminApp\wwwroot\Products.json";
+        public const string ReactJson = @"D:\Magnum-Enterprise\ReactApp\magnum-app\src\data\products.json";
         public const string FileName = "products.json";
     }
 }
